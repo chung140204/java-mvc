83e76625f9cc6f5b2f9425679bfa54192ea00339
chung140204 <170853492+chung140204@users.noreply.github.com>
2025-03-16 00:30:07 +0700

    update

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude =
+org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+        ApplicationContext abc = SpringApplication.run(DemoApplication.class, args);
+for(String s: abc.getBeanDefinitionNames())
+{
+System.out.println(s);
+}
 	}
 
 }
